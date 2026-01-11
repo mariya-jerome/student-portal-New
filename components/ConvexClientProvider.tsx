23aa63c740e2bac227e20dc40f4f552c3ef07225
Jerome <mariyajerome159@gmail.com>
2026-01-11 21:16:07 +0530

    Fix Convex deployment and dashboard runtime error

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -2,18 +2,18 @@
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-const convex = new ConvexReactClient(
-  process.env.NEXT_PUBLIC_CONVEX_URL!
-);
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL;
+
+if (!convexUrl) {
+  throw new Error("NEXT_PUBLIC_CONVEX_URL is not defined");
+}
+
+const convex = new ConvexReactClient(convexUrl);
 
 export default function ConvexClientProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <ConvexProvider client={convex}>
-      {children}
-    </ConvexProvider>
-  );
+  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
 }
